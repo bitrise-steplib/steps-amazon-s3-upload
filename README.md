b9c6e4f43d18d7329d02ab2958cbc0d95e6d957e
Viktor Benei <viktor.benei@gmail.com>
2014-06-06 03:06:18 +0200

    S3 ACL option

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ It will remove every other folder from the bucket!
 - AWS_SECRET_ACCESS_KEY
 - S3_UPLOAD_BUCKET
 - S3_UPLOAD_LOCAL_PATH : if you want to sync only the content of the folder, but don't want to create the folder then you should append a slas at the end of the path. Example: ./folder/
+- S3_ACL_CONTROL : can be 'public-read' or 'private'. 'private' is the default.
 
 
 # Note
--- step.sh
+++ step.sh
@@ -31,6 +31,14 @@ print_and_do_command brew install s3cmd
 print_and_do_command s3cmd --version
 printf %"s\n" '[default]' "access_key = $AWS_ACCESS_KEY_ID" "secret_key = $AWS_SECRET_ACCESS_KEY" > $HOME/.s3cfg
 
-print_and_do_command s3cmd sync "$S3_UPLOAD_LOCAL_PATH" "s3://$S3_UPLOAD_BUCKET" --delete-removed --acl-public
+s3_url="s3://$S3_UPLOAD_BUCKET"
+print_and_do_command s3cmd sync "$S3_UPLOAD_LOCAL_PATH" "$s3_url" --delete-removed
+
+aclcmd='--acl-private'
+if [ "$S3_ACL_CONTROL" == 'public-read' ]; then
+  echo " (i) ACL 'public-read' specified!"
+  aclcmd='--acl-public'
+fi
+s3cmd setacl "$s3_url" $aclcmd --recursive
 
 rm $HOME/.s3cfg
